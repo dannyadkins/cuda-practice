b4b65b099791cb2d686cb764a5cb19fc42e91b17
dannyadkins <danielpadk@gmail.com>
2024-01-30 05:36:07 +0000

    broken imports ugh

--- cv/rgb_to_grayscale/main.py
+++ cv/rgb_to_grayscale/main.py
@@ -1,8 +1,29 @@
+import sys
 from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 import torch
 from torchvision.io import read_image, write_png
 from torch.utils.cpp_extension import load_inline
-from utils.compile_extension import compile_extension
+# from utils.compile_extension import compile_extension
+
+def compile_extension(file_name, cpp_fn_signature, cpp_fn_name):
+    if (Path(file_name).suffix != ".cu"):
+        raise Exception("File must be a .cu file")
+
+    cuda_source = Path(file_name).read_text()
+
+    # Load the CUDA kernel as a PyTorch extension
+    ext = load_inline(
+        name=file_name.split(".")[0],
+        cpp_sources=cpp_fn_signature,
+        cuda_sources=cuda_source,
+        functions=[cpp_fn_name],
+        with_cuda=True,
+        extra_cuda_cflags=["-O2"],
+        # build_directory='./cuda_build',
+    )
+    return ext
 
 
 def main():
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        return req.read().splitlines()
+
+
+setup(
+    name='cuda-practice',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=read_requirements()
+)
